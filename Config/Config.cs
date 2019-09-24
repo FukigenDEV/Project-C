f1c25409dabc39d915a330abf35fd2519d4c80ac
Nolz <36479504+TehNolz@users.noreply.github.com>
2019-09-24 21:33:35 +0200

    Add .gitignore Add logging module Add configuration file module

---
+++ Config/Config.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Configurator {
+	public static class Config {
+		private static string DefaultConfigString;
+		private static JObject LoadedConfig;
+
+		/// <summary>
+		/// Saves a default config file. If the specified file already exists, nothing will happen unless Overwrite is true.
+		/// </summary>
+		/// <param name="File"></param>
+		public static void SaveDefaultConfig(string Path = "Config.json", bool Overwrite = false) {
+			if (!File.Exists(Path) || Overwrite) {
+				using (StreamWriter ConfigFile = File.CreateText(Path)) {
+					ConfigFile.Write(DefaultConfigString);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Load the specified config file.
+		/// </summary>
+		/// <param name="File">Path to the config file.</param>
+		public static Dictionary<string, List<string>> LoadConfig(string Path = "Config.json") {
+			if (!File.Exists(Path)) {
+				throw new FileNotFoundException(Path + " does not exist.");
+			}
+
+			//Parse JSON
+			JObject Config = JObject.Parse(File.ReadAllText(Path));
+
+			//Get differences, if any
+			Dictionary<string, List<string>> Missing = new Dictionary<string, List<string>>();
+			Dictionary<string, Dictionary<string, object>> DefaultConfigKeys = JObject.Parse(DefaultConfigString).ToObject<Dictionary<string, Dictionary<string, object>>>();
+			Dictionary<string, Dictionary<string, object>> ConfigKeys = Config.ToObject<Dictionary<string, Dictionary<string, object>>>();
+			foreach (string Key in DefaultConfigKeys.Keys) {
+				if (!ConfigKeys.ContainsKey(Key)) {
+					Missing.Add(Key, new List<string>(DefaultConfigKeys[Key].Keys.ToList()));
+				} else {
+					foreach (string Key2 in DefaultConfigKeys[Key].Keys) {
+						if (!ConfigKeys[Key].ContainsKey(Key2) || ConfigKeys[Key][Key2].GetType() != DefaultConfigKeys[Key][Key2].GetType()) {
+							Missing.TryAdd(Key, new List<string>());
+							Missing[Key].Add(Key2);
+						}
+					}
+				}
+			}
+
+			LoadedConfig = Config;
+
+			return (Missing);
+		}
+
+		/// <summary>
+		/// Retrieves a configuration setting.
+		/// </summary>
+		/// <param name="Setting">The setting to retrieve.</param>
+		/// <returns></returns>
+		public static dynamic GetVal(string Setting) {
+			string[] Pair = Setting.Split('.');
+			return LoadedConfig.GetValue(Pair[0]).ToObject<JObject>().GetValue(Pair[1]).ToObject(LoadedConfig.GetValue(Pair[0]).ToObject<JObject>().GetValue(Pair[1]).GetType());
+		}
+
+		/// <summary>
+		/// Adds external configuration (eg. from a plugin or module) to the global configuration.
+		/// After adding all external configs, run Config.LoadConfig()
+		/// </summary>
+		/// <param name="ExtConfig"></param>
+		public static void AddConfig(StreamReader Reader) {
+			if(DefaultConfigString == null) {
+				DefaultConfigString = Reader.ReadToEnd();
+			} else {
+				DefaultConfigString = DefaultConfigString.Remove(DefaultConfigString.Length - 3) + "," + Reader.ReadToEnd().Remove(0, 1);
+			}
+		}
+	}
+}
---
+++ Webserver/Program.cs
@@ -0,0 +1,29 @@
+﻿using Configurator;
+using Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace Webserver {
+	class Program {
+		public static void Main(string[] _) {
+			Logger.Init();
+			Logger Log = new Logger();
+
+			Config.AddConfig(new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("Webserver.DefaultConfig.json")));
+			Config.SaveDefaultConfig();
+			Dictionary<string, List<string>> Missing = Config.LoadConfig();
+
+			//TODO Properly show invalid config options
+			foreach (string Key in Missing.Keys) {
+				if (Missing[Key].Count == 0) { Console.Write(Key); }
+				foreach (string Key2 in Missing[Key]) {
+					Console.WriteLine(Key + "." + Key2);
+				}
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
