50c569991cf7264686023b9d00eb98b9b01b1475
Mike van der Burg <0969441@hr.nl>
2019-10-21 15:08:39 +0200

    Routing: Added 404 route

--- ReactJSDashboard/src/components/main.jsx
+++ ReactJSDashboard/src/components/main.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {Error} from '../index';
 
 class Main extends Component {
   getPointer = () => {
@@ -16,11 +17,13 @@ class Main extends Component {
 
   render() {
     const {navs} = this.props;
+    console.log(this.props.router);
 
     return (
       <div className="col-9 main-window">
         <Switch>
           { navs.map(nav => (<Route exact path={nav.path} component={nav.component} />)) }
+          <Route component={Error} />
         </Switch>
       </div>
     );
