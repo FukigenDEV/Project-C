b934b13f1784399444cca7aaf883aad7d6bc447f
Nolz <36479504+TehNolz@users.noreply.github.com>
2019-10-26 19:49:02 +0200

    Don't send an error page if an API method isn't defined. The client isn't going to care about it anyway.

--- Webserver/APIEndpoint.cs
+++ Webserver/APIEndpoint.cs
@@ -18,53 +18,55 @@ namespace Webserver {
 		/// Called when a HTTP.GET request is sent to this endpoint.
 		/// A GET request should only be used to retrieve data from the server.
 		/// </summary>
-		public virtual void GET() => Utils.Send(Response, Utils.GetErrorPage(HttpStatusCode.MethodNotAllowed), HttpStatusCode.MethodNotAllowed);
+		public virtual void GET() => Utils.Send(Response, null, HttpStatusCode.MethodNotAllowed);
 
 		/// <summary>
 		/// Called when a HTTP.HEAD request is sent to this endpoint.
 		/// HEAD requests ask for the same response as GET requests do, but without a response body.
 		/// </summary>
-		public virtual void HEAD() => Utils.Send(Response, Utils.GetErrorPage(HttpStatusCode.MethodNotAllowed), HttpStatusCode.MethodNotAllowed);
+		public virtual void HEAD() => Utils.Send(Response, null, HttpStatusCode.MethodNotAllowed);
+
 		/// <summary>
 		/// Called when a HTTP.POST request is sent to this endpoint.
 		/// POST requests are used to submit new data to the server.
 		/// </summary>
-		public virtual void POST() => Utils.Send(Response, Utils.GetErrorPage(HttpStatusCode.MethodNotAllowed), HttpStatusCode.MethodNotAllowed);
+		public virtual void POST() => Utils.Send(Response, null, HttpStatusCode.MethodNotAllowed);
+
 		/// <summary>
 		/// Called when a HTTP.PUT request is sent to this endpoint.
 		/// PUT requests are used to replace data on the server.
 		/// </summary>
-		public virtual void PUT() => Utils.Send(Response, Utils.GetErrorPage(HttpStatusCode.MethodNotAllowed), HttpStatusCode.MethodNotAllowed);
+		public virtual void PUT() => Utils.Send(Response, null, HttpStatusCode.MethodNotAllowed);
 
 		/// <summary>
 		/// Called when a HTTP.DELETE request is sent to this endpoint.
 		/// DELETE requests are used to delete data from the server.
 		/// </summary>
-		public virtual void DELETE() => Utils.Send(Response, Utils.GetErrorPage(HttpStatusCode.MethodNotAllowed), HttpStatusCode.MethodNotAllowed);
+		public virtual void DELETE() => Utils.Send(Response, null, HttpStatusCode.MethodNotAllowed);
 
 		/// <summary>
 		/// Called when a HTTP.CONNECT request is sent to this endpoint.
 		/// A CONNECT request is used to establish a tunnel to the server, identified by the targeted resource
 		/// </summary>
-		public virtual void CONNECT() => Utils.Send(Response, Utils.GetErrorPage(HttpStatusCode.MethodNotAllowed), HttpStatusCode.MethodNotAllowed);
+		public virtual void CONNECT() => Utils.Send(Response, null, HttpStatusCode.MethodNotAllowed);
 
 		/// <summary>
 		/// Called when a HTTP.OPTIONS request is sent to this endpoint.
 		/// An OPTIONS request is used to describe the communication options for the target resource.
 		/// </summary>
-		public virtual void OPTIONS() => Utils.Send(Response, Utils.GetErrorPage(HttpStatusCode.MethodNotAllowed), HttpStatusCode.MethodNotAllowed);
+		public virtual void OPTIONS() => Utils.Send(Response, null, HttpStatusCode.MethodNotAllowed);
 
 		/// <summary>
 		/// Called when a HTTP.TRACE request is sent to this endpoint.
 		/// A TRACE request is used to perform a message loop-back test along the path to the target resource.
 		/// </summary>
-		public virtual void TRACE() => Utils.Send(Response, Utils.GetErrorPage(HttpStatusCode.MethodNotAllowed), HttpStatusCode.MethodNotAllowed);
+		public virtual void TRACE() => Utils.Send(Response, null, HttpStatusCode.MethodNotAllowed);
 
 		/// <summary>
 		/// Called when a HTTP.PATCH request is sent to this endpoint.
 		/// A PATH request is used to partially modify data.
 		/// </summary>
-		public virtual void PATCH() => Utils.Send(Response, Utils.GetErrorPage(HttpStatusCode.MethodNotAllowed), HttpStatusCode.MethodNotAllowed);
+		public virtual void PATCH() => Utils.Send(Response, null, HttpStatusCode.MethodNotAllowed);
 
 		/// <summary>
 		/// Sends data to the client, answering the request.
