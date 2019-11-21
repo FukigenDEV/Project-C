e289c2f50aa2c3c4fe8d940233bbd475bec1f9bb
Nolz <36479504+TehNolz@users.noreply.github.com>
2019-11-22 00:50:30 +0100

    Add data endpoint. Add data GET endpoint method

---
+++ Webserver/API Endpoints/Data/GetData.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Webserver.Data;
+
+namespace Webserver.API_Endpoints.DataTable {
+	[EndpointURL("/data")]
+	partial class Data : APIEndpoint {
+
+		[RequireContentType("application/json")]
+		public override void GET() {
+			//Get required fields
+			if (!RequestParams.ContainsKey("table")) {
+				Send("Missing fields", HttpStatusCode.BadRequest);
+				return;
+			}
+			int Begin = 0;
+			if (RequestParams.ContainsKey("begin")) {
+				int.TryParse(RequestParams["begin"][0], out Begin);
+			}
+			int End = 25;
+			if (RequestParams.ContainsKey("begin")) {
+				int.TryParse(RequestParams["begin"][0], out End);
+			}
+			bool isUnvalidated = false;
+			if (RequestParams.ContainsKey("isvalidated")){
+				bool.TryParse(RequestParams["isvalidated"][0], out isUnvalidated);
+			}
+
+			//Check if all specified tables exist
+			if (!GenericDataTable.Exists(Connection, RequestParams["table"])) {
+				Send("No such table", HttpStatusCode.NotFound);
+				return;
+			}
+
+			//Create response JSON
+			JObject Response = new JObject();
+			foreach(string TableName in RequestParams["table"]) {
+				GenericDataTable Table = GenericDataTable.GetTableByName(Connection, TableName);
+				Response.Add(TableName, isUnvalidated? Table.GetUnvalidatedRows(Begin, End) : Table.GetRows(Begin, End));
+			}
+
+			Send(Response, HttpStatusCode.OK);
+		}
+	}
+}
