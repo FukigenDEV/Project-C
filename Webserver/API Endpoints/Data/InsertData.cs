3331fe96e73e141e469fe587cb4a6d343eb5ca70
Nolz <36479504+TehNolz@users.noreply.github.com>
2019-11-22 13:17:37 +0100

    Add /data POST (insert data into table)

---
+++ Webserver/API Endpoints/Data/InsertData.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Webserver.Data;
+
+namespace Webserver.API_Endpoints {
+	partial class Data : APIEndpoint {
+
+		[RequireContentType("application/json")]
+		public override void POST() {
+			//Get required fields
+			if (!RequestParams.ContainsKey("table")) {
+				Send("Missing params", HttpStatusCode.BadRequest);
+				return;
+			}
+
+			//Check if all specified table exist
+			GenericDataTable Table = GenericDataTable.GetTableByName(Connection, RequestParams["table"][0]);
+			if (Table == null) {
+				Send("No such table", HttpStatusCode.NotFound);
+				return;
+			}
+
+			//Build insertion dict
+			List<string> Columns = Table.GetColumns().Keys.ToList();
+			Dictionary<string, dynamic> Dict = new Dictionary<string, dynamic>();
+			foreach(KeyValuePair<string, JToken> Entry in JSON) {
+				if (!Columns.Contains(Entry.Key)) {
+					Send("No such column: " + Entry.Key);
+					return;
+				}
+				Dict.Add(Entry.Key, Entry.Value);
+			}
+
+			//Insert data and return response
+			Table.Insert(Dict);
+			Send(HttpStatusCode.Created);
+		}
+	}
+}
