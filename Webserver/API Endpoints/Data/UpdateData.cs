d47a959001f397d18cb1475c173e7784dd89c73f
Nolz <36479504+TehNolz@users.noreply.github.com>
2019-11-22 19:08:51 +0100

    Add /data PATCH (update datatable rows)

---
+++ Webserver/API Endpoints/Data/UpdateData.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Webserver.Data;
+
+namespace Webserver.API_Endpoints {
+	partial class Data : APIEndpoint {
+
+		[RequireContentType("application/json")]
+		public override void PATCH() {
+			//Get required fields
+			if (!RequestParams.ContainsKey("table")) {
+				Send("Missing params", HttpStatusCode.BadRequest);
+				return;
+			}
+
+			//Check if all specified table exist
+			GenericDataTable Table = GenericDataTable.GetTableByName(Connection, RequestParams["table"][0]);
+			if (Table == null) {
+				Send("No such table", HttpStatusCode.NotFound);
+				return;
+			}
+
+			//Convert JSON to update dict
+			Dictionary<int, Dictionary<string, dynamic>> Dict = new Dictionary<int, Dictionary<string, dynamic>>();
+			Dictionary<string, DataType> Columns = Table.GetColumns();
+			foreach(KeyValuePair<string, JToken> Entry in JSON) {
+				if(!int.TryParse(Entry.Key, out int RowID)) {
+					Send("Invalid row ID");
+					return;
+				}
+				if(Entry.Value.Type != JTokenType.Object) {
+					Send("Invalid row data");
+					return;
+				}
+				Dict.Add(RowID, new Dictionary<string, dynamic>());
+				foreach (KeyValuePair<string, JToken> Column in (JObject)Entry.Value) {
+					if (Columns.ContainsKey(Column.Key)) {
+						if(Columns[Column.Key] == DataType.Integer && Column.Value.Type != JTokenType.Integer) {
+							Send("Invalid datatype (" + Column.Key + " should be Integer", HttpStatusCode.BadRequest);
+							return;
+						}
+						Dict[RowID].Add(Column.Key, Columns[Column.Key] == DataType.Integer? (int)Column.Value : (dynamic)Column.Value);
+					} else {
+						Send("Unknown column "+Column.Key, HttpStatusCode.BadRequest);
+						return;
+					}
+				}
+			}
+			Table.Update(Dict);
+			Send(HttpStatusCode.OK);
+		}
+	}
+}
