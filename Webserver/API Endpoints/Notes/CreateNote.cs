156557e9641d2c88ab14621dc936d2c864223630
nhazeu1 <47557049+nhazeu1@users.noreply.github.com>
2019-11-28 12:14:01 +0100

    Replaces Content with JSON

--- Webserver/API Endpoints/Notes/CreateNote.cs
+++ Webserver/API Endpoints/Notes/CreateNote.cs
@@ -13,9 +13,9 @@ namespace Webserver.API_Endpoints
         public override void POST()
         {
             // Get all required values
-            if (!Content.TryGetValue<string>("title", out JToken title) ||
-                !Content.TryGetValue<string>("text", out JToken text) ||
-                !Content.TryGetValue<string>("author", out JToken author))
+            if (!JSON.TryGetValue<string>("title", out JToken title) ||
+                !JSON.TryGetValue<string>("text", out JToken text) ||
+                !JSON.TryGetValue<string>("author", out JToken author))
             {
                 Send("Missing fields", HttpStatusCode.BadRequest);
                 return;
