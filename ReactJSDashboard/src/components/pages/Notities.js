33fb2124e71fc9877ab63e7c98e8b4a631435bb1
Naomi van Duijn <naomivanduijn@live.nl>
2019-12-02 15:00:49 +0100

    Alle notities leesbaar en met wrapping.

--- ReactJSDashboard/src/components/pages/Notities.js
+++ ReactJSDashboard/src/components/pages/Notities.js
@@ -1,89 +1,104 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
+import './Style/Notities.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Notities extends Component {
-  componentDidMount() {
-	$("#notitieAanmakenKnop").on("click", function(event) {
-		$("#alleNotities").hide(250);
-		$("#notitieAanmaken").show(250);
-	});
-	$("#alleNotitiesKnop").on("click", function(event) {
-		$("#notitieAanmaken").hide(250);
-		$("#alleNotities").show(250);
-	});
-
-	$("#add_note_form").on("submit", function(event) {
-		event.preventDefault();
-
-		var title = $("#note_title").val();
-		var text = $("#note_text").val();
-
-		var xhr = new XMLHttpRequest();
-		
-		xhr.open("POST", "/note", true);
-		xhr.setRequestHeader("Content-Type", "application/json");
-
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState === 4) {
-				if (xhr.status === 200) {
-					$("#message").html(xhr.responseText);
-				}
-			}
-		}
-		
-		var data = JSON.stringify({"title": title, "text": text});
-		xhr.send(data);
-	});
-		
-	var xhr = new XMLHttpRequest();
-		
-	xhr.open("GET", "/note?title=", true);
-	xhr.setRequestHeader("Content-Type", "application/json");
-
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === 4) {
-			$("#all_notes").html(xhr.responseText);
+	constructor(props) {
+		super(props)
+		this.state = {
+			allNotes: []
 		}
 	}
+	componentDidMount() {
+		$("#notitieAanmakenKnop").on("click", function (event) {
+			$("#alleNotities").hide(250);
+			$("#notitieAanmaken").show(250);
+		});
+		$("#alleNotitiesKnop").on("click", function (event) {
+			$("#notitieAanmaken").hide(250);
+			$("#alleNotities").show(250);
+		});
 
-	xhr.send();
-  }
+		$("#add_note_form").on("submit", function (event) {
+			event.preventDefault();
 
-  render() {
-    return (
-      <div className="shadow-sm p-3 mb-5 bg-white rounded">
-        <h2>Notities</h2>
-		<hr/>
+			var title = $("#note_title").val();
+			var text = $("#note_text").val();
 
-		<button type="button" id = "alleNotitiesKnop" class="btn btn-outline-primary" style={{display: "inline-block"}}>
-			Alle notities</button>
-			
-		<button type="button" id= "notitieAanmakenKnop" class="btn btn-outline-primary" style={{display: "inline-block"}}>
-			Notitie aanmaken</button>
+			var xhr = new XMLHttpRequest();
+
+			xhr.open("POST", "/note", true);
+			xhr.setRequestHeader("Content-Type", "application/json");
+
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState === 4) {
+					if (xhr.status === 200) {
+						$("#message").html(xhr.responseText);
+					}
+				}
+			}
+
+			var data = JSON.stringify({ "title": title, "text": text });
+			xhr.send(data);
+		});
+		this.getAllNotes();
+	}
+	getAllNotes = () => {
+		fetch('/note?title=')
+			.then(notes => {
+				return notes.json();
+			}).then(allNotes => {
+				this.setState({ allNotes });
+				console.log(this.state.allNotes)
+			})
+	}
+
+	render() {
+		return (
+			<div className="shadow-sm p-3 mb-5 bg-white rounded">
+				<h2>Notities</h2>
+				<hr />
+
+				<button type="button" id="alleNotitiesKnop" class="btn btn-outline-primary" style={{ display: "inline-block" }}>
+					Alle notities</button>
+
+				<button type="button" id="notitieAanmakenKnop" class="btn btn-outline-primary" style={{ display: "inline-block" }}>
+					Notitie aanmaken</button>
 
 
-		<div id ="alleNotities">
-			<h5><b>Alle notities</b></h5>
-			<p id="all_notes"></p>
-		</div>
+				<div id="alleNotities">
+					<h5><b>Alle notities</b></h5>
+					<div className="all_notes">
+						{this.state.allNotes.map((Note, index) => {
+							return <div className="full_note">
+								<h4>{Note.Title}</h4>
+								<p>
+									{Note.Text}
+								</p>
+							</div>
+						})}
+					</div>
 
-		<div id="notitieAanmaken" style= {{display: "none"}}>
-			<h5><b>Notitie toevoegen</b></h5>
-			<form id="add_note_form" method="POST">
-				<label>Titel:</label><br/>
-				<input id="note_title" type="text" name="note_title" style={{width: "50%", marginBottom: "15px"}}/><br/>
-				<label>Tekst:</label><br/>
-				<input id="note_text" type="text" name="note_text" style={{width: "50%", height: "225px"}}/><br/>
-				<br/>
-				<input type="submit" value="Toevoegen" style={{width: "200px"}}/><br/><br/>
+				</div>
 
-				<p id="message"></p>
-			</form>
-		</div>
+				<div id="notitieAanmaken" style={{ display: "none" }}>
+					<h5><b>Notitie toevoegen</b></h5>
+					<form id="add_note_form" method="POST">
+						<label>Titel:</label><br />
+						<input id="note_title" type="text" name="note_title" style={{ width: "50%", marginBottom: "15px" }} /><br />
+						<label>Tekst:</label><br />
+						<input id="note_text" type="text" name="note_text" style={{ width: "50%", height: "225px" }} /><br />
+						<br />
+						<input type="submit" value="Toevoegen" style={{ width: "200px" }} /><br /><br />
 
-      </div>
-    );
-  }
+						<p id="message"></p>
+					</form>
+				</div>
+
+			</div>
+		);
+	}
 }
 
 export default Notities;
