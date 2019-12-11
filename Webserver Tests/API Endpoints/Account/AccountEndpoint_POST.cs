df1dead598aa29f274809470aac69afc76263539
Nolz <36479504+TehNolz@users.noreply.github.com>
2019-12-11 21:01:19 +0100

    Unit test tweaks

--- Webserver Tests/API Endpoints/Account/AccountEndpoint_POST.cs
+++ Webserver Tests/API Endpoints/Account/AccountEndpoint_POST.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -28,43 +29,44 @@ namespace Webserver.API_Endpoints.Tests {
 			Assert.IsTrue(Account.GetPermissionLevel(Connection, 2) == PermLevel.Manager);
 		}
 
+		[SuppressMessage("Code Quality", "IDE0051")]
+		static IEnumerable<object[]> InvalidTestData => new[]{
+			new object[] {
+				new JObject() {
+					{"Email", "SomeEmail"},
+					{"Password", "examplepassword12345"},
+					{"MemberDepartments", new JObject() {
+						{"All Users", "Manager" }
+					} }
+				},
+				HttpStatusCode.BadRequest,
+				"Invalid email"
+			},
+			new object[] {
+				new JObject() {
+					{"Email", "user@example.com"},
+					{"Password", "a"},
+					{"MemberDepartments", new JObject() {
+						{"All Users", "Manager" }
+					}}
+				},
+				HttpStatusCode.BadRequest,
+				"Password does not meet requirements"
+			}
+		};
+
 		/// <summary>
-		/// Check if we get a BadRequest if we specify an invalid email
+		/// Check invalid arguments
 		/// </summary>
 		[TestMethod]
-		public void POST_InvalidEmail() {
-			ResponseProvider Response = ExecuteSimpleRequest("/account", HttpMethod.POST, new JObject() {
-				{"Email", "SomeEmail"},
-				{"Password", "examplepassword12345"},
-				{"MemberDepartments", new JObject() {
-					{"All Users", "Manager" }
-				}}
-			});
-
-			Assert.IsTrue(Response.StatusCode == HttpStatusCode.BadRequest);
-			Assert.IsTrue(Encoding.UTF8.GetString(Response.Data) == "Invalid email");
+		[DynamicData("InvalidTestData")]
+		public void POST_InvalidArguments(JObject Request, HttpStatusCode StatusCode, string ResponseMsg) {
+			ResponseProvider Response = ExecuteSimpleRequest("/account", HttpMethod.POST, Request);
+			Assert.IsTrue(Response.StatusCode == StatusCode);
+			string Result = Encoding.UTF8.GetString(Response.Data);
+			Assert.IsTrue(Result == ResponseMsg);
 		}
 
-		/// <summary>
-		/// Check if we get a BadRequest if we specify an invalid password
-		/// </summary>
-		[TestMethod]
-		public void POST_InvalidPassword() {
-			ResponseProvider Response = ExecuteSimpleRequest("/account", HttpMethod.POST, new JObject() {
-				{"Email", "user@example.com"},
-				{"Password", "a"},
-				{"MemberDepartments", new JObject() {
-					{"All Users", "Manager" }
-				}}
-			});
-
-			Assert.IsTrue(Response.StatusCode == HttpStatusCode.BadRequest);
-			Assert.IsTrue(Encoding.UTF8.GetString(Response.Data) == "Password does not meet requirements");
-		}
-
-		/// <summary>
-		/// Check if we get a BadRequest if we use an email that already exists.
-		/// </summary>
 		[TestMethod]
 		public void POST_AlreadyExists() {
 			new User("user@example.com", "SomePassword", Connection);
