b0e0d32e2cd6c939088531b6865bede66c1170b2
Naomi van Duijn <naomivanduijn@live.nl>
2019-12-16 12:21:42 +0100

    comments bij notities en icon

--- ReactJSDashboard/src/components/pages/Notities.js
+++ ReactJSDashboard/src/components/pages/Notities.js
@@ -3,11 +3,12 @@ import $ from 'jquery';
 import './Style/Notities.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashAlt, fas } from '@fortawesome/free-solid-svg-icons';
+import { faTrashAlt, fas, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 library.add(
 	fas,
-	faTrashAlt
+	faTrashAlt,
+	faPencilAlt
 )
 
 
@@ -18,27 +19,33 @@ class Notities extends Component {
 			allNotes: []
 		}
 	}
+	// nadat het component aangemaakt is, wordt de componentDidMount altijd als eerste aangeroepen
 	componentDidMount() {
-		$("#notitieAanmakenKnop").on("click", function (event) {
+		$("#notitieAanmakenKnop").on("click", function () {
 			$("#alleNotities").hide(250);
 			$("#notitieAanmaken").show(250);
 		});
-		$("#alleNotitiesKnop").on("click", function (event) {
+		$("#alleNotitiesKnop").on("click", function () {
 			$("#notitieAanmaken").hide(250);
 			$("#alleNotities").show(250);
 		});
-
+		// Wanneer de gebruiker zijn tekst heeft ingevoerd en op de submit knop drukt...
 		$("#add_note_form").on("submit", function (event) {
+			// Wordt de pagina NIET herladen...
 			event.preventDefault();
-
+			// En worden de ingetypte waardes in een variabel gezet
 			var title = $("#note_title").val();
 			var text = $("#note_text").val();
 
+			// Maak een nieuwe XML request naar de server
 			var xhr = new XMLHttpRequest();
 
+			// de open method initialiseerd de request, in de backend zoekt hij naar de filename "note", en daarin wordt data gePOST (gegeven aan server), true voor async uitvoeren
 			xhr.open("POST", "/note", true);
+			// Geeft aan de XHR door dat hij json data moet verwachten
 			xhr.setRequestHeader("Content-Type", "application/json");
 
+			// als hij klaar is (4) en als dan de status code 200 is (goed) dan geeft hij een response tekst vanuit de server, dit bevestigd dat je notitie is toegevoegd
 			xhr.onreadystatechange = function () {
 				if (xhr.readyState === 4) {
 					if (xhr.status === 200) {
@@ -46,31 +53,42 @@ class Notities extends Component {
 					}
 				}
 			}
-
+			// Omdat de request asynchroon wordt uitgevoerd, wordt de data hieronder omgezet in JSON data, en daarna opgestuurd
 			var data = JSON.stringify({ "title": title, "text": text });
 			xhr.send(data);
 		});
+		// Aan het einde wordt deze functie aangeroepen omdat anders de pagina herladen moest worden om de nieuwe notitie te laten zien
 		this.getAllNotes();
 	}
+	// Deze functie haalt alle gemaakte notities op van de server
 	getAllNotes = () => {
+		// in de /note (endpoint URL) in de backend gaat hij zoeken naar alle notes die gemaakt zijn, dit doet hij aan de hand van de titel omdat geen note dezelfde titel mag hebben dus werkt als ID
 		fetch('/note?title=')
 			.then(notes => {
 				return notes.json();
+				// Als er eventueel een note bij is gekomen (of afgegaan) dan veranderd hij dit in de state en dit wordt dan weer gerendered op je scherm
 			}).then(allNotes => {
 				this.setState({ allNotes });
 				console.log(this.state.allNotes)
 			})
 	}
+	// notitie verwijderen
 	deleteNote = async (name) => {
+		// omdat hij async is kan de fetch met await worden gebruikt zodat hij tussendoor eventueel doorkan met een andere functie
 		await fetch('/note', {
+			// in de note endpoint zoekt hij naar de DELETE functie
 			method: 'DELETE',
+			// zet de titel in json formaat
 			body: JSON.stringify({ title: name }),
+			// Geeft door dat hij json data moet verwachten 
 			headers: {
 				'Content-Type': 'application/json'
 			}
 		});
+		// en weer na verwijderen de notities opnieuw ophalen zodat de verandering op je scherm verschijnt
 		this.getAllNotes();
 	}
+	// Ik heb een klein pop up scherm die standaard in je browser zit gebruikt om te bevestigen dat men daadwerkelijk de notitie verwijderd
 	deleteConfirm = (title) => {
 		if (window.confirm("Weet u zeker dat u deze notitie wilt verwijderen?")) {
 			this.deleteNote(title)
@@ -93,7 +111,7 @@ class Notities extends Component {
 				<div id="alleNotities">
 					<h5><b>Een overzicht van alle notities:</b></h5>
 					<div className="all_notes">
-						{this.state.allNotes.map((Note, index) => {
+						{this.state.allNotes.map(Note => {
 							return <div className="full_note note_border">
 								<h6>{Note.Title}</h6>
 								<a class="note_delete" onClick={() => this.deleteConfirm(Note.Title)}><FontAwesomeIcon icon={['fas', 'trash-alt']} /></a>
